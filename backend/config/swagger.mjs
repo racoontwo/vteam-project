d102425883ce79df420edc3a27fc63f2190fa135
Pontus Karlsson <Pontuswkarlsson@gmail.com>
2024-12-11 15:36:20 +0100

    installed swagger-jsdoc and swagger-ui-express. Implemented functionality for swagger documentation and wrote swagger comments for the existing routes.

---
+++ backend/config/swagger.mjs
@@ -0,0 +1,22 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "API Documentation",
+            version: "1.0.0",
+            description: "Documentation of the REST API for the project",
+        },
+        servers: [
+            {
+                url: "http://localhost:5001", // Ändra till din bas-URL
+            },
+        ],
+    },
+    apis: ["./src/routes/v1/*.mjs"], // Peka på filerna där du skriver Swagger-kommentarer
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export default { swaggerSpec };
