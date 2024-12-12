05965f9a7ff28827cef9aa7061254452b5d99c16
racoon2 <oliver.roosvall@gmail.com>
2024-12-12 13:43:34 +0100

    First commit for the simulation folder.

--- simulation/app.js
+++ simulation/app.js
@@ -1,1 +1,17 @@
-console.log('Hello from simulation');
+//this would be the simulation-app in the end.
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import database from './modules/scooter_db.js';
+import Scooter from './scooter.js';
+
+let result = await database.getAllScooters('scooters');
+console.log(result);
+
+async function main() {
+    // const scooter = new Scooter(3, randomCoordinates);
+    // scooter.getAllInfo();
+}
+
+main();
---
+++ simulation/scooter.js
@@ -0,0 +1,37 @@
+
+import database from './modules/scooter_db.js';
+
+let currentId = 1; // Initialize the starting ID
+
+export default function Scooter(id = currentId++, location = {}) { 
+    this._id = id; // Assign the ID to this scooter
+    this.location = location;
+    this.status = "Off";
+    this.rented = false;
+    this.battery = Math.floor(Math.random() * 101);
+
+
+    this.getStatus = function () {
+        if (this.battery <= 0) {
+            this.status = "Off";
+        } else if (this.battery <= 10) {
+            this.status = "Low battery";
+        } else {
+            this.status = "On";
+        }
+        return this.status;
+    };
+    
+
+    this.getAllInfo = function () {
+        console.log("Scooter Id", this._id);
+        console.log("Battery:", this.battery);
+        console.log("Location:", this.location);
+        console.log("Status:", this.status);
+        console.log("Rented?:", this.rented);
+
+    }
+
+}
+
+
