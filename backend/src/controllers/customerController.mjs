661a51f2c88f3f7e080dc336e000f281d15f3aac
Pontus Karlsson <Pontuswkarlsson@gmail.com>
2024-12-16 09:38:25 +0100

    Added routes, controllers and test to delete customers.

--- backend/src/controllers/customerController.mjs
+++ backend/src/controllers/customerController.mjs
@@ -3,29 +3,71 @@ import { ObjectId } from 'mongodb';
 
 const customers = {
     getAllCustomers: async function getAllCustomers() {
+        let db;
         try {
             const db = await database.getCollection('customers');
             const result = await db.collection.find({}).toArray();
 
-            await db.client.close();
             return result;
         } catch (error) {
         console.error('Error fetching customers:', error);
         throw new Error('Failed to fetch customers');
+        } finally {
+            if (db) {
+                await db.client.close(); // Ensure the client is always closed
+            }
         }
     },
-    addCustomer: async function addCustomer({ firstName, lastName }) {
+
+    addCustomer: async function addCustomer(data) {
+        let db;
         try {
             const db = await database.getCollection('customers');
-            const result = await db.collection.insertOne({ firstName, lastName });
+            const result = await db.collection.insertOne(data);
 
-            await db.client.close();
             return result;
         } catch (error) {
             console.error('Failed to insert data into database:', error);
             throw new Error('Database insertion failed' + error.message);
+        } finally {
+            if (db) {
+                await db.client.close(); // Ensure the client is always closed
+            }
         }
     },
+
+    deleteOneCustomer: async function deleteOneCustomer(id) {
+        let db;
+        try {
+            const db = await database.getCollection('customers');
+            const result = await db.collection.deleteOne({ _id: new ObjectId(id) });
+
+            return result;
+        } catch (error) {
+            console.error('Error deleting customer from database:', error);
+            throw new Error('Customer deletion failed');
+        } finally {
+            if (db) {
+                await db.client.close(); // Ensure the client is always closed
+            }
+        }
+    },
+
+    deleteAllCustomers: async function deleteAllCustomers() {
+        let db;
+        try {
+            const db = await database.getCollection('customers');
+            const result = await db.collection.deleteMany({});
+            return result;
+        } catch (error) {
+            console.error('Error deleting data from MongoDB:', error);
+            throw new Error('Database deletion failed');
+        } finally {
+            if (db) {
+                await db.client.close(); // Ensure the client is always closed
+            }
+        }
+    }
 }
 
 export default customers;
--- backend/src/routes/v1/customer.mjs
+++ backend/src/routes/v1/customer.mjs
@@ -30,7 +30,7 @@ const router = express.Router();
 
 /**
  * @swagger
- * /all-customers:
+ * /api/v1/customers/all-customers:
  *   get:
  *     summary: Retrieve all customers
  *     tags: [Customers]
@@ -69,7 +69,7 @@ router.get('/all-customers', async (req, res) => {
 
 /**
  * @swagger
- * /add-customer:
+ * /api/v1/customers/add-customer:
  *   post:
  *     summary: Add a new customer
  *     tags: [Customers]
@@ -128,17 +128,110 @@ router.get('/all-customers', async (req, res) => {
  *                   type: string
  */
 router.post('/add-customer', async (req, res) => {
-    const { firstName, lastName } = req.body;
-    if (!firstName || !lastName) {
-        return res.status(400).json({ error: 'Full name is required' });
-    }
+    const data = req.body;
+    // if (!data.firstName || !data.lastName) {
+    //     return res.status(400).json({ error: 'Full name is required' });
+    // }
 
     try {
-        const result = await customers.addCustomer({ firstName, lastName });
-        res.status(200).json({ message: 'Data received and inserted', data: { firstName, lastName }, result });
+        const result = await customers.addCustomer(data);
+        res.status(200).json({ message: 'Data received and inserted', data: data, result });
     } catch (error) {
         console.error('Error occurred during customer insertion:', error);
         res.status(500).json({ error: 'Failed to insert data into database', details: error.message });
     }
 });
+
+/**
+ * @swagger
+ * /api/v1/customers/delete-all-customers:
+ *   delete:
+ *     summary: Delete all customers from the collection
+ *     description: Removes all customer records from the database.
+ *     tags:
+ *       - Customers
+ *     responses:
+ *       200:
+ *         description: Successfully deleted all customers
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: All customers deleted successfully
+ *                 result:
+ *                   type: object
+ *                   properties:
+ *                     acknowledged:
+ *                       type: boolean
+ *                       example: true
+ *                     deletedCount:
+ *                       type: integer
+ *                       example: 10
+ *       500:
+ *         description: Failed to delete customers
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Failed to delete customers
+ *                 error:
+ *                   type: string
+ *                   example: An error occurred while deleting customers
+ */
+router.delete('/delete-all-customers', async (req, res) => {
+    try {
+        const result = await customers.deleteAllCustomers();
+        res.status(200).json({ message: 'All customers deleted successfully', result });
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to delete customers', error: error.message });
+    }
+});
+
+/**
+ * @swagger
+ * /api/v1/customers/delete-one-customer:
+ *   delete:
+ *     summary: Deletes a customer by ID
+ *     description: Deletes a customer record from the database using the provided customer ID.
+ *     tags: [Customers]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               _id:
+ *                 type: string
+ *                 description: The ID of the customer to be deleted.
+ *                 example: "60d72b2f9b1d8a7a1c7d56e7"
+ *     responses:
+ *       200:
+ *         description: Customer deleted successfully
+ *       404:
+ *         description: Customer not found
+ *       500:
+ *         description: Failed to delete customer due to an internal error
+ */
+router.delete('/delete-one-customer', async (req, res) => {
+    const customerID = req.body._id;
+    try {
+        const result = await customers.deleteOneCustomer(customerID);
+
+        if (result.deletedCount === 1) {
+            res.status(200).json({ message: "Customer deleted successfully" });
+        } else {
+            res.status(404).json({ message: "Customer not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Failed to delete customer" });
+    }
+});
+
 export default router;
