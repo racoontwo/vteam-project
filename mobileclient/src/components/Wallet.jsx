a0f11eb378eaa2e41db0a646c7a9a5c50978ed65
Teodor29 <teodor.lien@gmail.com>
2024-12-16 23:29:12 +0100

    Update Wallet component to fetch customer data and display balance; modify App component to pass customerId

--- mobileclient/src/components/Wallet.jsx
+++ mobileclient/src/components/Wallet.jsx
@@ -1,11 +1,49 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
-function wallet({ onLogout }) {
+function Wallet({ customerId }) {
+    const [customer, setCustomer] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const baseUrl = import.meta.env.VITE_API_BASE_URL;
+
+    useEffect(() => {
+        if (!customerId) {
+            setError('Customer ID is required');
+            setLoading(false);
+            return;
+        }
+
+        const fetchCustomer = async () => {
+            try {
+                const response = await fetch(`${baseUrl}/customers/${customerId}`);
+                if (!response.ok) {
+                    throw new Error('Failed to fetch customer');
+                }
+                const data = await response.json();
+                setCustomer(data);
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchCustomer();
+    }
+    , [customerId, baseUrl]);
+
     return (
         <div className="wallet">
-            <h1>wallet</h1>
+            <h2>Wallet</h2>
+            {loading && <p>Loading...</p>}
+            {error && <p>{error}</p>}
+            {customer && (
+                <div>
+                    <h1>{customer.balance} Kr</h1>
+                </div>
+            )}
         </div>
     );
 }
 
-export default wallet;
+export default Wallet;
