ff3798f2e319d244cf92c34167866a089ce80862
Teodor29 <teodor.lien@gmail.com>
2024-12-30 23:14:49 +0100

    Reused code from customers component to display scooters in a list

--- admin-webclient/src/components/Scooters.jsx
+++ admin-webclient/src/components/Scooters.jsx
@@ -1,4 +1,67 @@
-function Scooters({ isLoggedIn, handleLogin, handleLogout }) {
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function Scooters({ isLoggedIn }) {
+    const [scooters, setScooters] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [expandedRows, setExpandedRows] = useState([]);
+    const baseUrl = "http://localhost:4000";
+    const apiKey = import.meta.env.VITE_API_KEY;
+
+    useEffect(() => {
+        const fetchCustomer = async () => {
+            try {
+                const response = await fetch(`${baseUrl}/scooters`, {
+                    headers: {  
+                    },
+                });
+
+                if (!response.ok) {
+                    throw new Error('Failed to fetch scooters');
+                }
+                const data = await response.json();
+                setScooters(data);
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchCustomer();
+    }, [baseUrl]);
+
+    const handleExpand = (ScooterId) => {
+        if (expandedRows.includes(ScooterId)) {
+            setExpandedRows(expandedRows.filter(id => id !== ScooterId));
+        } else {
+            setExpandedRows([...expandedRows, ScooterId]);
+        }
+    };
+
+    const handleDelete = async (ScooterId) => {
+        try {
+            const response = await fetch(`${baseUrl}/api/v1/scooters/delete-one-scooter`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-api-key': apiKey,
+                },
+                body: JSON.stringify({ id: ScooterId }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to delete scooter');
+            }
+
+            setScooters({ data: scooters.data.filter(scooter => scooter.id !== ScooterId) });
+
+        } catch (error) {
+            setError(error.message);
+        }
+    };
+
     if (!isLoggedIn) {
         return (
             <div className="scooters">
@@ -6,9 +69,51 @@ function Scooters({ isLoggedIn, handleLogin, handleLogout }) {
             </div>
         );
     }
+
     return (
         <div className="scooters">
-            <h1>Scooters</h1>
+            <div className="scooters-header">
+                <h1>Scooters</h1>
+                <Link to="/add-scooter">
+                    <button>Add scooter</button>
+                </Link>
+            </div>
+            {loading && <h2>Loading...</h2>}
+            {error && <p>{error}</p>}
+            {scooters && (
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Status</th>
+                            <th>Battery</th>
+                            <th>Speed</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {scooters.map(scooter => (
+                            <React.Fragment key={scooter.id}>
+                                <tr onClick={() => handleExpand(scooter.id)}>
+                                    <td>{scooter.id}</td>
+                                    <td>{scooter.status}</td>
+                                    <td>{scooter.battery}</td>
+                                    <td>{scooter.speed}</td>
+                                </tr>
+                                {expandedRows.includes(scooter.id) && (
+                                    <tr className="expanded">
+                                        <td colSpan="3">
+                                            <div className="expanded-content">
+                                                <p>{scooter.status} {scooter.battery}</p>
+                                                <button onClick={() => handleDelete(scooter.id)}>Delete</button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                )}
+                            </React.Fragment>
+                        ))}
+                    </tbody>
+                </table>
+            )}
         </div>
     );
 }
