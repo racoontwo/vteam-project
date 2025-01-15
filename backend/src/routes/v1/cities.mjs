601065ec2a635e05192bef0ea2f088ebf29367c3
racoon2 <oliver.roosvall@gmail.com>
2025-01-15 12:19:52 +0100

    first try to get routes working. in backend

---
+++ backend/src/routes/v1/cities.mjs
@@ -0,0 +1,33 @@
+import express from 'express';
+import cities from '../../controllers/adminController.mjs';
+
+const router = express.Router();
+
+router.get('/all-cities', async (req, res) => {
+    try {
+        const data = await cities.getAllCities();
+        res.status(200).json({ data });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Failed to fetch data',
+            error: error.message
+        });
+    }
+});
+
+router.get('/city/:cityName', async (req, res) => {
+    const cityName = req.params.cityName;
+
+    try {
+        const data = await admins.getCity(cityName);
+        res.status(200).json({ data });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Failed to fetch data',
+            error: error.message
+        });
+    }
+});
+
+
+export default router;
