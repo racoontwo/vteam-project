2b64d5196c0e02474802786f81c93b1929f684fc
Teodor29 <teodor.lien@gmail.com>
2025-01-15 12:17:38 +0100

    add leaflet and react-leaflet for map integration; update Home component to display scooters on the map

--- mobileclient/src/context/CustomerContext.jsx
+++ mobileclient/src/context/CustomerContext.jsx
@@ -8,7 +8,8 @@ export const CustomerProvider = ({ customerId, children }) => {
     const [customer, setCustomer] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
-    const baseUrl = import.meta.env.VITE_BASE_URL;
+    // const baseUrl = import.meta.env.VITE_BASE_URL;
+    const baseUrl = "http://localhost:4000";
 
     useEffect(() => {
         if (!customerId) {
@@ -19,12 +20,12 @@ export const CustomerProvider = ({ customerId, children }) => {
 
         const fetchCustomer = async () => {
             try {
-                const response = await fetch(`${baseUrl}/customers/${customerId}`);
+                const response = await fetch(`${baseUrl}/customers?_id=${customerId}`);
                 if (!response.ok) {
                     throw new Error('Failed to fetch customer');
                 }
                 const data = await response.json();
-                setCustomer(data);
+                setCustomer(data[0]);
             } catch (error) {
                 setError(error.message);
             } finally {
