1035f0072af2efabc76e77aea5f4c0af6d0ce03d
racoon2 <oliver.roosvall@gmail.com>
2025-01-16 14:25:32 +0100

    increased api request limit

--- backend/src/middleware/rateLimit.mjs
+++ backend/src/middleware/rateLimit.mjs
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const rateLimiter = rateLimit({
     windowMs: 5 * 60 * 1000, // 5 minutes
-    max: 100, // Limit each API key or IP to 100 requests per windowMs
+    max: 1000, // Limit each API key or IP to 100 requests per windowMs
     message:
         'Too many requests from this API key or IP. Please try again later.',
     standardHeaders: true, // Send rate limit info in headers
